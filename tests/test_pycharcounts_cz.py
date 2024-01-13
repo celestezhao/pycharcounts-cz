4b836ccd8ce7ca639f547681c340d045f59651c2
Celeste Zhao <emilyxxzhao@outlook.com>
2024-01-13 15:32:48 -0800

    test: add test

--- tests/test_pycharcounts_cz.py
+++ tests/test_pycharcounts_cz.py
@@ -1,1 +1,8 @@
-from pycharcounts_cz import pycharcounts_cz
+from pycharcounts_cz.pycharcounts_cz import count_characters
+
+def test_count_characters():
+    """Test character counting from a text file."""
+    expected = 1289
+    path = "tests/Can We Touch Your Hair.txt"
+    actual = count_characters(path)
+    assert actual == expected, "The characters in the poem 'Can We Touch Your Hair' counted incorrectly!"
